88a3150ff51e2ba325005329efa67970e4b7d9f6
Sabrina Tran <sabrina@Sabrinas-MacBook-Air.local>
2022-11-29 18:24:49 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import cutekitties from './cutekitties.png';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [clicked, setClicked] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={cutekitties} className="App-logo" alt="logo" />
+        <button 
+          className='Start-button'
+          onClick={() => setClicked(!clicked)}
+        >Start
+        </button>
+        <p className='Title'>This or That</p>
+        {clicked && <p>YAY</p>}
       </header>
     </div>
   );
