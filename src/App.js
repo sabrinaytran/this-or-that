15093d87b7ceda40e512e698d693bf0720bbd631
Sabrina Tran <sabrinaytran@gmail.com>
2022-12-08 23:59:38 -0800

    Added firestore

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import { useState } from 'react';
 import './App.css';
 import Choices from './components/Choices';
 import Startpage from './components/Startpage';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from 'uuid';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEZB2QdYSXtufV-6QwKCC8VBarb7_DlEM",
+  authDomain: "this-or-that-4a63c.firebaseapp.com",
+  projectId: "this-or-that-4a63c",
+  storageBucket: "this-or-that-4a63c.appspot.com",
+  messagingSenderId: "730589566561",
+  appId: "1:730589566561:web:6c3c53b6f4143229f2fc20",
+  measurementId: "G-N9MYNGC5KM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const addData = async (choiceOne, choiceTwo) => {
+  // Add a new document in collection "responses"
+  await setDoc(doc(db, "responses", uuidv4()), {
+    option1: choiceOne,
+    option2: choiceTwo,
+  });
+}
+
 
 function App() {
   const [clicked, setClicked] = useState(false);
